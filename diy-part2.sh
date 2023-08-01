a0526c55fe6160145ab8c87046b0b6c6323ee2ea
GYJ668 <139406938+GYJ668@users.noreply.github.com>
2023-08-01 19:40:44 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,17 +11,17 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
-sed -i "s/hostname='OpenWrt'/hostname='Phicomm-K3'/g" package/base-files/files/bin/config_generate
+# sed -i "s/hostname='OpenWrt'/hostname='Phicomm-K3'/g" package/base-files/files/bin/config_generate
 
 # Change Default Theme
 
 # Add Screenctrl
-rm -rf package/lean/k3screenctrl
-rm -rf package/lean/luci-app-k3screenctrl
-git clone https://github.com/lwz322/luci-app-k3screenctrl.git package/lean/luci-app-k3screenctrl
-git clone https://github.com/lwz322/k3screenctrl_build.git package/lean/k3screenctrl
-wget -nv https://github.com/393435992/k3screen-fix-patch/raw/main/k3screen/000-k3screen.patch  -P package/lean/k3screenctrl/patches
+# rm -rf package/lean/k3screenctrl
+# rm -rf package/lean/luci-app-k3screenctrl
+# git clone https://github.com/lwz322/luci-app-k3screenctrl.git package/lean/luci-app-k3screenctrl
+# git clone https://github.com/lwz322/k3screenctrl_build.git package/lean/k3screenctrl
+# wget -nv https://github.com/393435992/k3screen-fix-patch/raw/main/k3screen/000-k3screen.patch  -P package/lean/k3screenctrl/patches
